edcf44e6384715fc2be6935d09fac7060b404b12
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2022-05-22 01:01:12 +0100

    Finished family view and the app as a whole

--- src/app/family/family.component.ts
+++ src/app/family/family.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-family',
   templateUrl: './family.component.html',
-  styleUrls: ['./family.component.sass']
+  styleUrls: ['./family.component.sass'],
 })
 export class FamilyComponent implements OnInit {
+  selectedMember = 0;
+  familyMembers = [
+    {
+      name: 'Mehdi',
+      description: 'Proud indian father, Angular developer and camel breeder',
+      image: '../../assets/images/Mehdi.jpeg',
+    },
+    {
+      name: 'Abdam',
+      description: 'Proud indian child and Buhdda follower',
+      image: '../../assets/images/Abdam.webp',
+    },
+    {
+      name: 'Anjali',
+      description: 'Proud indian wife and professional Rissotto cooker',
+      image: '../../assets/images/Anjali.jpg',
+    },
+    {
+      name: 'Brownie',
+      description: 'Proud indian child and Buhdda follower',
+      image: '../../assets/images/Brownie.gif',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  move(value: number) {
+    if (this.selectedMember + value < 0) {
+      this.selectedMember = 3;
+    } else if (this.selectedMember + value > 3) {
+      this.selectedMember = 0;
+    } else {
+      this.selectedMember += value;
+    }
   }
 
+  ngOnInit(): void {}
 }
